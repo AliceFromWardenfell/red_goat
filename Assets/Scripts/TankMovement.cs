6eb286e0784881086fc70da6aa823dd51ddf6d5a
AliceFromWardenfell <wosemkg@gmail.com>
2022-05-05 16:45:42 +0300

    Add moveable tank composed of blocks

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
+
+    private Rigidbody m_Rigidbody;
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+    private void OnEnable()
+    {
+        m_Rigidbody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+    private void OnDisable()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        m_MovementInputValue = Input.GetAxis("Vertical");
+        m_TurnInputValue = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        Vector3 NewMovement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+        m_Rigidbody.MovePosition(m_Rigidbody.position + NewMovement);
+    }
+
+    private void Turn()
+    {
+        float NewTurn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+        Quaternion TurnRotation = Quaternion.Euler(0f, NewTurn, 0f);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * TurnRotation);
+    }
+}
